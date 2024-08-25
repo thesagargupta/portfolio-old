37f0399de3f1757d989058eec9c44c477ef46927
sagar gupta <sagarkshn8@gmail.com>
2024-08-25 21:58:28 +0530

    / route

--- myformapp/server.js
+++ myformapp/server.js
@@ -39,14 +39,6 @@ app.post('/submit', (req, res) => {
     .catch(err => res.status(400).json({ success: false, error: err }));
 });
 
-app.get('/',(req, res) => {
-  res.send({
-
-    activeStatus:true,
-    error:false,
-
-  })
-})
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
