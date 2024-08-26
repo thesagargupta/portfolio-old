6e34be28229c1fc2fa0c78e56cb85a50885bdfd0
sagar gupta <sagarkshn8@gmail.com>
2024-08-26 23:39:36 +0530

    updating api

--- client/config.js
+++ client/config.js
@@ -1,6 +1,8 @@
 // config.js
+
+// Use a default URL if the environment variable is not set
 const config = {
-   API_URL: 'https://portfolio-nine-gilt-63.vercel.app' // Your Vercel backend URL
+    API_URL: process.env.API_URL || 'http://localhost:3000' // Default to local if not defined
 };
 
 window.config = config;
