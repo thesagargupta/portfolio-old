401ae76ea4f133d158fd3230d6d686a7dcb44215
sagar gupta <sagarkshn8@gmail.com>
2024-08-26 22:05:10 +0530

    port

--- myformapp/server.js
+++ myformapp/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
