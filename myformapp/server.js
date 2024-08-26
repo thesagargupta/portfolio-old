9ffb94c6d82d57dcb95cde4aadcc9cd5494b4273
sagar gupta <sagarkshn8@gmail.com>
2024-08-26 20:59:08 +0530

    cors

--- myformapp/server.js
+++ myformapp/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const cors = require('cors');
 app.use(cors({
   origin: 'https://sagarguptaportfolio.netlify.app'  // Update with your frontend URL
 }));
