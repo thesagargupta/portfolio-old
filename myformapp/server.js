15f7a9224a0cc0e3b6b78ae5fa75fed1d3d23d19
sagar gupta <sagarkshn8@gmail.com>
2024-08-27 17:41:19 +0530

    hosting

--- myformapp/server.js
+++ myformapp/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://sagarguptaportfolio.netlify.app' // Set to your frontend URL
+  origin: process.env.CLIENT_URL || 'http://localhost:8000' // Set to your frontend URL
 }));
 
 // MongoDB Atlas connection string
