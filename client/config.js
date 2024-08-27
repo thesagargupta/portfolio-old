95737be5acc909c9561862129b145bbc9aa3c5a1
sagar gupta <sagarkshn8@gmail.com>
2024-08-27 17:36:46 +0530

    api inti

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'https://sagarguptaportfolio.netlify.app'
+    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'https://portfolio-nine-gilt-63.vercel.app/'
 };
 
 window.config = config;
