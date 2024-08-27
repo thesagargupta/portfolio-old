3921a4bdc6dfe2fc10673cca0eff0c475001265b
sagar gupta <sagarkshn8@gmail.com>
2024-08-27 18:55:27 +0530

    updation

--- client/config.js
+++ client/config.js
@@ -1,6 +1,5 @@
-// config.js
 const config = {
-    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'http://localhost:5000'
+    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'https://portfolio-nine-gilt-63.vercel.app'
 };
 
 window.config = config;
--- client/script.js
+++ client/script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('myForm').addEventListener('submit', handleSubmit);
 });
