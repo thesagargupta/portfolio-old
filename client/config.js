2d83180abbb9f6abd3169db0a783686c723c54fc
sagar gupta <sagarkshn8@gmail.com>
2024-08-27 18:27:49 +0530

    api ch

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    API_URL: 'https://portfolio-nine-gilt-63.vercel.app' // Replace with your deployed backend URL
+    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'http://localhost:5000'
 };
 
 window.config = config;
