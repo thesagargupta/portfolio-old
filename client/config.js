a0249f39a3024f7603574faece7b6a4b0e12d5e5
sagar gupta <sagarkshn8@gmail.com>
2024-08-27 17:20:03 +0530

    server emove

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'http://localhost:3000'
+    API_URL: window.env && window.env.API_URL ? window.env.API_URL : 'https://sagarguptaportfolio.netlify.app'
 };
 
 window.config = config;
