4b5510760b56e4cecd4e2840e4e9c76c882ad072
sagar gupta <sagarkshn8@gmail.com>
2024-12-03 18:45:41 +0530

    uploading

--- client/side.js
+++ client/side.js
@@ -1,8 +1,15 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-  }
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-  }
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+  
+  // Add event listener to hide the sidebar when an option is clicked
+  const menuOptions = sidebar.querySelectorAll('li');
+  menuOptions.forEach(option => {
+    option.addEventListener('click', hideSidebar);
+  });
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
